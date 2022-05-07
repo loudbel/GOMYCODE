ebed7c55a61f0ac081e72097ddcb302e2ba651e5
BELOUD <amine.beoud@gmail.com>
2022-05-07 14:35:12 +0100

    ok

---
+++ java script/exercices/Exercises_simple/exo3/exo3.js
@@ -0,0 +1,22 @@
+//--------------------------------------------------- EXO 3 ---------------------------------------------------
+
+/*
+
+- Write a script that prompt the user to enter base and height of the triangle and calculate an area of a 
+  triangle (area = 0.5 x b x h).
+- Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate 
+  the perimeter of triangle (perimeter = a + b + c).
+- Get length and width using prompt and calculate an area of rectangle (area = length x width) and the perimeter 
+  of rectangle (perimeter = 2 x (length + width)).
+- Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle 
+  (circumference = 2 x pi x r) where pi = 3.14.
+
+*/
+function TriangleArea(b,h){
+    return area = 0.5 * b * h ;
+}
+
+console.log(10,5); 
+k
+
+

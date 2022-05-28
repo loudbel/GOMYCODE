5c5661bfbc345fa8fb9bde441f6d79da10f08320
BELOUD <amine.beoud@gmail.com>
2022-05-28 12:44:57 +0100

    after prof check

--- DOM/exercices dom/memory_game/script.js
+++ DOM/exercices dom/memory_game/script.js
@@ -20,7 +20,9 @@ let cards = document.querySelectorAll('.card_face.card_front i')
 //ajouter la class icone au back card
 for (let i = 0; i < shuffle.length; i++) {
   cards[i].classList.add(shuffle[i]);
+  document.getElementsByClassName('card')[i].setAttribute('card', i);
   document.getElementsByClassName('card')[i].addEventListener("click", turnCard);
+  //document.getElementsByClassName('card')[i].addEventListener("click", compareCards);
 }
 //fin de start
 };
@@ -32,10 +34,75 @@ if (document.readyState === "loading") {
   start();
 }
 
+
+
 //tourner la carte après click
-//document.getElementsByClassName('card').addEventListener("click", turnCard);
 function turnCard() {
-  var element = this.document.querySelector('.card_face.card_front');
-  element.classList.remove("card_front");
+   let el = this;
+   if(el.classList.contains('visible'))return;
+   
+   //if(compare.length === 2)return;
+   el.classList.add("visible");
+   compareCards(el);
+   initComp();
+}
+
+
+
+var compare = [];
+var lastClicked = [];
+
+function compareCards(el){
+  //let el = this;
+
+  //id de l'element clické
+  var id = el.getAttribute('card');
+ 
+  var laClass = el.querySelector('.card_face.card_front i').className;
+  //si plus que deux cliqué retourner
   
+  //si compare plus que deux , réinitialiser
+  if(compare.length >= 2){
+    compare = [];
+    if(lastClicked.length > 2){
+      document.getElementsByClassName('card')[lastClicked[0]].classList.remove("visible");
+      document.getElementsByClassName('card')[lastClicked[1]].classList.remove("visible");
+    }
+    lastClicked = [];
+  }
+  
+  //si meme index ne pas comparer
+  if(compare.length <2 || lastClicked[0] != lastClicked[1]){
+    compare.push(laClass);
+  }
+  lastClicked.push(id);
+
+console.log(compare);
+console.log(lastClicked);
+if(compare.length == 2 && compare[0] === compare[1] && lastClicked[0] != lastClicked[1]){
+
+  console.log('match');
+  document.querySelector('[card="'+lastClicked[0]+'"]').classList.add("visible");
+  document.querySelector('[card="'+lastClicked[1]+'"]').classList.add("visible");
+
+} else if(lastClicked.length >= 2) {
+  setTimeout(
+    function() {
+      document.querySelector('[card="'+lastClicked[0]+'"]').classList.remove("visible");
+      document.querySelector('[card="'+lastClicked[1]+'"]').classList.remove("visible");
+    }, 1000);
+}
+}
+
+
+function initComp(){
+  console.log(compare.length);
+  if(compare.length > 2){
+    compare = [];
+    if(lastClicked.length > 2){
+      document.getElementsByClassName('card')[lastClicked[0]].classList.remove("visible");
+      document.getElementsByClassName('card')[lastClicked[1]].classList.remove("visible");
+    }
+    lastClicked = [];
+  }
 }

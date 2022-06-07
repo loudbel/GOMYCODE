269be4100b7f0a1fcaeea7c2386080f5795a8a03
Amine <amine.beloud@gmail.com>
2022-06-07 16:53:27 +0100

    dom checkpoint

---
+++ DOM/SHOPING CART/cart.js
@@ -0,0 +1,46 @@
+let products = [
+    {
+        name : "product 1",
+        description : "Description produit 1",
+        price : 150,
+        fav : false,
+    },
+    {
+        name : "product 2",
+        description : "Description produit 2",
+        price : 230,
+        fav : true,
+    },
+    {
+        name : "product 3",
+        description : "Description produit 3",
+        price : 499,
+        fav : false,
+    }
+];
+let vat = 0.19;
+
+//lister produits sur la page html
+function listProducts(){
+
+}
+
+//ajouter le produit cliqué a la carte
+function addToCart(id){
+
+}
+
+//supprimer le produit cliqué a la carte
+function delFromCart(id){
+
+}
+
+//ajouter produit aux favoris
+function addFav(id){
+
+}
+
+//calculer le total des produits avec tva
+function totalCart(){
+
+}
